a1411707b630774644551d8331d43d967a29d809
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-13 13:59:38 +0000

    Set up Karma to run Chrome in Travis

--- test/unit/setup/karma.conf.js
+++ test/unit/setup/karma.conf.js
@@ -1,8 +1,11 @@
 const webpackConfig = require('./webpack.config.test.js');
 
+const travis = process.env.TRAVIS;
+
 module.exports = function karmaConfig(config) {
   config.set({
-    browsers: ['PhantomJS', 'Chrome'],
+    // If running in travis, use custom chrome
+    browsers: travis ? ['PhantomJS', 'Chrome_travis_ci'] : ['PhantomJS', 'Chrome'],
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: ['../specs/*.{vue,js}'],
@@ -19,6 +22,12 @@ module.exports = function karmaConfig(config) {
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' },
       ],
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
     },
   });
 };
