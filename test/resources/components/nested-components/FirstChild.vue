6bf2ad4213a5ac35455e8f528848462c9de286cd
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-16 20:07:42 +0000

    Add nested Vue components for testing

---
+++ test/resources/components/nested-components/FirstChild.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+	import SecondChild from './SecondChild.vue';
+
+  export default{
+    name: 'FirstChild',
+    components: {
+      SecondChild,
+    },
+  };
+</script>
---
+++ test/resources/components/nested-components/Parent.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<FirstChild />
+	</div>
+</template>
+
+<script>
+	import FirstChild from './FirstChild.vue';
+
+  export default{
+    name: 'Parent',
+    components: {
+      FirstChild,
+    }
+  };
+</script>
