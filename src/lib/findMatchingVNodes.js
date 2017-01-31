d3510f79f44591f4fd0b67ea4dab6c0264a92f51
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-31 10:55:47 +0000

    Add support for descendant combinators

--- src/lib/findMatchingVNodes.js
+++ src/lib/findMatchingVNodes.js
@@ -1,5 +1,12 @@
-function findAllVNodes(vNode, nodes = []) {
-  nodes.push(vNode);
+function getSelectors(selector) {
+  const selectors = selector.split(/ (?=(?:(?:[^"]*"){2})*[^"]*$)/);
+  return selectors.reduce((list, sel) => list.concat(sel), []);
+}
+
+function findAllVNodes(vNode, nodes = [], first) {
+  if (!first) {
+    nodes.push(vNode);
+  }
 
   if (vNode.children && vNode.children.length > 0) {
     for (let i = 0; i < vNode.children.length; i++) { // eslint-disable-line no-plusplus
@@ -14,13 +21,33 @@ function findAllVNodes(vNode, nodes = []) {
   return nodes;
 }
 
-export default function findMatchingVNodes(vNode, selector) {
-  const nodes = findAllVNodes(vNode);
+function getMatchingNodes(vNode, selector, first) {
+  const nodes = findAllVNodes(vNode, [], first);
   return nodes.filter((node) => {
     if (node.elm && node.elm.matches) {
       return node.elm.matches(selector);
     }
     return false;
   });
+}
+
+function recurseGetMatchingVNodes(vNodes, selectors, first) {
+  const nodes = [];
+
+  vNodes.forEach(node => nodes.push(...getMatchingNodes(node, selectors[0], first)));
+
+  if (selectors.length <= 1) {
+    return nodes;
+  }
+
+  return recurseGetMatchingVNodes(nodes, selectors.splice(0, 1), true);
+}
+
+export default function findMatchingVNodes(vNode, selector) {
+  const selectorsArray = getSelectors(selector);
+  if (selectorsArray.length > 1) {
+    return recurseGetMatchingVNodes([vNode], selectorsArray);
+  }
+  return getMatchingNodes(vNode, selector);
 }
 
--- test/unit/specs/findMatchingVNodes.spec.js
+++ test/unit/specs/findMatchingVNodes.spec.js
@@ -1,77 +1,57 @@
 import findMatchingVNodes from '../../../src/lib/findMatchingVNodes';
 import '../../../src/lib/matchesPolyfill';
 
-function divWithAttribute(attributes) {
-  const element = document.createElement('div');
+function elementWithAttribute(tag, attributes) {
+  const element = document.createElement(tag);
   Object.keys(attributes).forEach((key) => {
     element.setAttribute(key, attributes[key]);
   });
   return element;
 }
 
-function divWithClass(className) {
-  const element = document.createElement('div');
+function elementWithClass(tag, className) {
+  const element = document.createElement(tag);
   element.className = className;
   return element;
 }
 
 const vNodeMock = {
-  elm: divWithClass('find'),
-  tag: 'div',
+  elm: elementWithClass('div', 'find'),
   child: {
     _vnode: {
       children: [
+        { elm: elementWithClass('div', 'find') },
         {
-          elm: divWithClass('find'),
-          tag: 'div',
-          child: undefined,
-          children: undefined,
-        },
-        {
-          elm: divWithAttribute({ attribute: 'value' }),
-          tag: 'div',
-          child: undefined,
-          children: undefined,
+          elm: elementWithAttribute('div', { attribute: 'value' }),
+          children: [{
+            elm: elementWithClass('p', 'some-class'),
+            children: [
+              {
+                elm: elementWithClass('p', 'another-class'),
+                children: [{ elm: elementWithClass('div', 'another-class') }] },
+            ],
+          }],
         },
       ],
     },
   },
   children: [
     {
-      elm: divWithAttribute({ id: 'id', attribute: 'some value' }),
-      tag: 'div',
-      child: undefined,
+      elm: elementWithAttribute('div', { id: 'id', attribute: 'some value' }),
       children: [
-        {
-          elm: divWithClass('find'),
-          tag: 'p',
-          children: undefined,
-        },
-        {
-          elm: divWithClass('find'),
-          tag: 'div',
-          child: undefined,
-          children: undefined,
-        },
-        {
-          elm: {
-            nodeName: '#text',
-          },
+        { elm: elementWithClass('div', 'find') },
+        { elm: elementWithClass('div', 'find') },
+        { elm: { nodeName: '#text' },
         },
       ],
     },
-    {
-      elm: divWithClass('this that'),
-      tag: 'div',
-      child: undefined,
-      children: undefined,
-    },
+    { elm: elementWithClass('div', 'this that') },
   ],
 };
 
 describe('findMatchingVNodes', () => {
   it('returns an array of vNodes of elements matching tag selector passed', () => {
-    expect(findMatchingVNodes(vNodeMock, 'div').length).to.equal(7);
+    expect(findMatchingVNodes(vNodeMock, 'div').length).to.equal(8);
   });
 
   it('returns an array of vNodes of elements matching className passed', () => {
@@ -96,5 +76,9 @@ describe('findMatchingVNodes', () => {
 
   it('returns an array of vNodes of elements matching id passed', () => {
     expect(findMatchingVNodes(vNodeMock, '#id').length).to.equal(1);
+  });
+
+  it('returns an array of vNodes of elements matching tag selector passed with descendant combinator', () => {
+    expect(findMatchingVNodes(vNodeMock, 'p p div').length).to.equal(1);
   });
 });
