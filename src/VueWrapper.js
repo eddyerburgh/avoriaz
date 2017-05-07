88bcc0403df49a75a53ebd5313fbdb9255701254
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-07 08:16:22 +0100

    [New] Re render in update method

--- src/VueWrapper.js
+++ src/VueWrapper.js
@@ -1,8 +1,13 @@
 import Wrapper from './Wrapper';
 
+function update() {
+  this.$forceUpdate.call(this);
+  this._update(this._render());
+}
+
 export default class VueWrapper extends Wrapper {
   constructor(vm, mountedToDom) {
-    super(vm._vnode, vm._watcher.run.bind(vm._watcher), mountedToDom);
+    super(vm._vnode, update.bind(vm), mountedToDom);
     this.vm = vm;
     Object.defineProperty(this, 'vNode', {
       get: () => this.vm._vnode,
