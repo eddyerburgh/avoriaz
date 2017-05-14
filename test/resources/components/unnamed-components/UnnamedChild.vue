99c76cd5e5ee3a0be01246e7b6a93c7c62011fee
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-14 09:19:49 +0100

    [Refactor] Use UnnamedChild component in validators unnamed test

---
+++ test/resources/components/unnamed-components/UnnamedChild.vue
@@ -0,0 +1,9 @@
+<template>
+  <div />
+</template>
+
+<script>
+  export default {
+    props: ['some-prop'],
+  };
+</script>
--- test/unit/specs/lib/validators.spec.js
+++ test/unit/specs/lib/validators.spec.js
@@ -1,7 +1,7 @@
 import { compileToFunctions } from 'vue-template-compiler';
 import { isVueComponent } from '../../../../src/lib/validators';
 import ClickComponent from '../../../resources/components/event-components/ClickComponent.vue';
-import SlotParent from '../../../resources/components/slots/SlotParent.vue';
+import UnnamedChild from '../../../resources/components/unnamed-components/UnnamedChild.vue';
 
 describe('isVueComponent', () => {
   it('returns true using a named .vue file', () => {
@@ -9,7 +9,7 @@ describe('isVueComponent', () => {
   });
 
   it('returns true using an unnamed .vue file', () => {
-    expect(isVueComponent(SlotParent)).to.equal(true);
+    expect(isVueComponent(UnnamedChild)).to.equal(true);
   });
 
   it('returns true using a compiled vue template', () => {
