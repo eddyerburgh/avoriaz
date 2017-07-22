814a19b76aa5b8e831ac65f9f581931bde360e34
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-22 21:40:46 +0100

    [Refactor] Fix formatting in WrapperInterface declaration

--- flow/wrapper.flow.js
+++ flow/wrapper.flow.js
@@ -3,28 +3,28 @@
 declare type Selector = string | Component
 
 declare interface WrapperInterface { // eslint-disable-line no-undef
-        computed(): Object;
-        contains(selector: Selector): boolean;
-        data(): Object;
-        destroy(): void;
-        dispatch(event: string): void;
-        hasAttribute(attribute: string, value: string): boolean;
-        hasClass(className: string): boolean;
-        hasStyle(style: string, value: string): boolean;
-        find(selector: Selector): WrapperType;
-        html(): string;
-        instance(): Component;
-        is(selector: Selector): boolean;
-        isEmpty(): boolean;
-        methods(): Object;
-        name(): string;
+    computed(): Object;
+    contains(selector: Selector): boolean;
+    data(): Object;
+    destroy(): void;
+    dispatch(event: string): void;
+    hasAttribute(attribute: string, value: string): boolean;
+    hasClass(className: string): boolean;
+    hasStyle(style: string, value: string): boolean;
+    find(selector: Selector): WrapperType;
+    html(): string;
+    instance(): Component;
+    is(selector: Selector): boolean;
+    isEmpty(): boolean;
+    methods(): Object;
+    name(): string;
     propsData(): Object,
-        text(): string;
-        setData(data: Object): void;
-        setProps(data: Object): void;
-        simulate(event: string): void;
-        style(): Object;
-        trigger(type: string, options: Object): void;
-        update(): void;
+    text(): string;
+    setData(data: Object): void;
+    setProps(data: Object): void;
+    simulate(event: string): void;
+    style(): Object;
+    trigger(type: string, options: Object): void;
+    update(): void;
 }
 
