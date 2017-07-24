61454248d42b567409aaa9b36ab5e667e0067303
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-24 10:33:42 +0100

    [Build] Revert to previous build

---
+++ index.js
@@ -0,0 +1,9 @@
+var mount = require('./dist/mount').default;
+var shallow = require('./dist/shallow').default;
+var use = require('./dist/use').default;
+
+module.exports = {
+    mount: mount,
+    shallow: shallow,
+    use: use
+};
